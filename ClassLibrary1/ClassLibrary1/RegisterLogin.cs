2b368ed54a4024f068c426f9c5e95b157d0df70f
deepakk2000 <deepakjc112000@gmal.com>
2024-01-08 19:35:22 +0530

    A-2

---
+++ ClassLibrary1/ClassLibrary1/RegisterLogin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class RegisterLogin
+    {
+        private List<UserAuthenticate> users;
+
+        public RegisterLogin()
+        {
+            // Initialize the list of users in the constructor
+            users = new List<UserAuthenticate>();
+        }
+
+        public bool Register(string user, string pass)
+        {
+            // Check if the username already exists
+            if (users.Any(u => u.Username == user))
+            {
+                Console.WriteLine("Username already exists. Registration failed.");
+                return false;
+            }
+
+            // Create a new user and add it to the list
+            UserAuthenticate newUser = new UserAuthenticate() { Username = user, Password = pass };
+            users.Add(newUser);
+
+            Console.WriteLine("Registration successful!");
+            return true;
+        }
+
+        public bool Login(string username, string password)
+        {
+            // Check if the username and password match any user in the list
+            foreach (var user in users)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    Console.WriteLine("Login Successful");
+                    return true;
+                }
+            }
+
+            Console.WriteLine("Login Failed");
+            return false;
+        }
+    }
+}
